5fd400448547122b103b920361f0c2435b8104cb
bstHoang <10t1qx4thaihoang@gmail.com>
2025-06-17 07:20:00 +0700

    add connon game

--- FlagQuiz/app/src/main/java/vn/edu/fpt/flagquiz/SettingsFragment.java
+++ FlagQuiz/app/src/main/java/vn/edu/fpt/flagquiz/SettingsFragment.java
@@ -1,4 +1,15 @@
 package vn.edu.fpt.flagquiz;
 import android.preference.PreferenceFragment;
-public class SettingsFragment extends PreferenceFragment{
-}
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsFragment extends PreferenceFragment
+{
+    // creates preferences GUI from preferences.xml file in res/xml
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences); // load from XML
+    }
+} // end class SettingsFragment
