d409ba1f40b2160fcdc0cb244bb22853621790e9
bstHoang <10t1qx4thaihoang@gmail.com>
2025-06-21 14:22:44 +0700

    update code 19/6/2025

---
+++ Doodlz/app/src/main/java/vn/edu/fpt/doodlz/DoodleView.java
@@ -0,0 +1,6 @@
+package vn.edu.fpt.doodlz;
+
+import android.app.Fragment;
+
+public class DoodleView extends Fragment {
+}
---
+++ Doodlz/app/src/main/java/vn/edu/fpt/doodlz/LineWidthDialogFragment.java
@@ -0,0 +1,6 @@
+package vn.edu.fpt.doodlz;
+
+import android.app.DialogFragment;
+
+public class LineWidthDialogFragment extends DialogFragment {
+}
